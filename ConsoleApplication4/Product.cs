5485ac7a2f161a281bb9564f3b1ff1235f598f69
snipe <snipe@RAIDEN>
2017-01-23 03:44:33 +0300

    Add project files.

---
+++ ConsoleApplication4/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4.model
+{
+    class Fruit
+    {
+        public override string ToString()
+        {
+            return name + "\t" + Quantity + "\t" + wieght;
+        }
+
+        public Fruit()
+        { }
+
+        public Fruit(string name, double wieght, int quantity)
+        {
+            Name = name;
+            Wieght = wieght;
+            Quantity = quantity;
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double wieght;
+        private double Wieght
+        {
+            get { return wieght; }
+            set { wieght = value; }
+        }
+        public int Quantity { set; get; }
+    }
+    class Vegetable : Fruit
+    {
+        public Vegetable()
+        { }
+        public Vegetable(string name, double wieght, int quantity) : base(name, wieght, quantity)
+        { }
+    }
+}
---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication4.model;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> inventory = new List<object>();
+
+            inventory.Add(new Fruit("apple", 5.4, 5));
+            inventory.Add(new Fruit("banana", 2.1, 2));
+            inventory.Add(new Vegetable("caroot", 6.6, 6));
+
+            Console.WriteLine(inventory.Count+"\n");
+
+            foreach (var item in inventory)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
